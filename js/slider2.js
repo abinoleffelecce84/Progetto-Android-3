72c47ad634b2e21220c544de2ddf7fa01c3e71ba
Paolo Vedorin <vedorinpaolo@gmail.com>
2017-09-04 14:50:28 +0200

    js update

--- js/slider2.js
+++ js/slider2.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-                $("#adds").hide(); 
-                $("#btn-create").click(function(){
-                   $("#adds").toggle("high"); 
-                });
-            });
+    
+    $("#adds").hide(); 
+    $("#btn-create").click(function(){
+        $("#adds").toggle("high"); 
+        if($("#change").is(":visible"))
+        {
+            $("#change").hide();
+        }
+    });
+});
